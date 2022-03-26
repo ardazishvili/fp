c3d035b6337bce8677ca201060a0bd3cc7b45c83
ardazishvili <ardazishvili.roman@yandex.ru>
2022-03-26 21:04:23 +0300

    Add class Task
    Support 2 use cases:
    * Initiate task with functon and arg
    * Initiate task with function only

--- task.hpp
+++ task.hpp
@@ -1,26 +1,53 @@
 #pragma once
 
+#include <cassert>
 #include <future>
+#include <memory>
+#include <optional>
+
+template <typename R, typename... Args>
+class Task {
+ public:
+  explicit Task(std::function<R(Args...)> f)
+      : task_(f), arg_(std::optional<Args...>()) {}
+
+  Task(std::function<R(Args...)> f, Args... arg) : task_(f), arg_(arg...) {}
+
+  auto get_result(auto arg) {
+    task_(arg);
+    return task_.get_future().get();
+  }
+
+  auto get_result() {
+    assert(arg_.has_value());
+    task_(arg_.value());
+    return task_.get_future().get();
+  }
+
+ private:
+  std::optional<Args...> arg_;
+  std::packaged_task<R(Args...)> task_;
+};
 
 template <typename T>
 auto compose(T&& t) {
-  return [&t](auto a) {
-    t(a);
-    return t.get_future().get();
-  };
+  return [&t](auto a) { return t.get_result(a); };
 }
 
 template <typename T, typename... Args>
 auto compose(T&& t, Args&&... args) {
   return [&t, ... args = std::forward<Args>(args)](auto a) mutable {
-    t(a);
-    auto ret = t.get_future().get();
+    auto ret = t.get_result(a);
     return compose(args...)(ret);
   };
 }
 
 template <typename R, typename... Args>
 auto make_task(std::function<R(Args...)> f) {
-  std::packaged_task<R(Args...)> task2(f);
-  return task2;
+  return Task(f);
+}
+
+template <typename R, typename... Args>
+auto make_task(std::function<R(Args...)> f, Args... arg) {
+  return Task(f, arg...);
 }
--- tests/task.cpp
+++ tests/task.cpp
@@ -3,12 +3,25 @@
 #include <gtest/gtest.h>
 
 #include <cmath>
+#include <functional>
 #include <future>
 #include <type_traits>
 
 std::function square = [](double x) { return ::sqrt(x); };
 
 std::function multiply2 = [](double x) { return x * 2; };
+
+std::function iden = [](double x) { return x; };
+
+TEST(TaskTest, IdentityInit) {
+  auto value = make_task(iden, 42.0).get_result();
+  ASSERT_EQ(value, 42.0);
+}
+
+TEST(TaskTest, IdentityNoInit) {
+  auto task = make_task(iden);
+  ASSERT_EQ(task.get_result(42.0), 42.0);
+}
 
 TEST(TaskTest, Compose) {
   auto value = compose(make_task(multiply2), make_task(square))(8);
